04185a611ec4dc2dc72a7cef76996773f8ef0143
Cailean Horton <caileanhorton@hotmail.com>
2024-04-09 10:41:45 -0500

    Initial sign up form produced, styles drastically improved and applied to login page as well. Sign up controller error checking added but not complete

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,40 @@
 class SignupController < ApplicationController
+
   def index
+
+    @new_email = params[:email]
+    @password = params[:password]
+    @verify_password = params[:password2]
+
+    if(@new_email == "")
+      flash[:error] = "No email provided"
+      return
+    end
+
+    existing_user = Users.find_by(email: @new_email)
+
+    if(existing_user.nil?)
+      if(@password != @verify_password)
+        flash[:error] = "Passwords must match."
+        return
+      end
+    else
+      flash[:error] = "Email already in use."
+      return
+    end
+
+    new_user = Users.create(
+      email: @new_email,
+      password: @password,
+      full_name: "New User",
+      address: "null",
+      provinces_id: 1
+    )
+
+    if(new_user.valid?)
+      redirect_to root_path, notice: "Logged In"
+    else
+
+    end
   end
 end
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,5 @@
 class Users < ApplicationRecord
   has_many :orders
   belongs_to :provinces, optional: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
