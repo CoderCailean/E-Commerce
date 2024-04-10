94f456e2e7a86f48ce6ae6c1d4af75b6924ff630
Cailean Horton <caileanhorton@hotmail.com>
2024-04-10 10:17:52 -0500

    Pre-Devise

---
+++ db/migrate/20240410150546_add_stripe_id_column.rb
@@ -0,0 +1,5 @@
+class AddStripeIdColumn < ActiveRecord::Migration[7.1]
+  def change
+    add_column :orders, :stripe_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_05_154513) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_150546) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_05_154513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.string "stripe_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
